0c200c045eb6f74176112cc11da91768f699d930
Harry McLaren <hazclan13@gmail.com>
2026-04-01 16:53:41 +0100

    feat: import os and re modules to verify_build.py for HTML parsing support

--- execution/verify_build.py
+++ execution/verify_build.py
@@ -1,6 +1,8 @@
 import sys
 import hashlib
 import base64
+import os
+import re
 
 # Compiled Regex for HTML parsing (Synced with build_site.py)
 SCRIPT_REGEX = re.compile(r'<script\b(?![^>]*\bsrc=)[^>]*>(.*?)</script>', re.IGNORECASE | re.DOTALL)
